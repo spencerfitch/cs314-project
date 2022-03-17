96e0021f72dd30cd46631d8aa3dc4e389afbec34
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2022-03-17 01:54:18 -0500

    Drawing canvas now repeats animation on interval

--- src/components/canvas/DisplayCanvas.js
+++ src/components/canvas/DisplayCanvas.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { createRef, useEffect } from 'react';
 import CanvasDraw from 'react-canvas-draw';
 
-const DisplayCanvas = ({ drawing, width, height, ...props }) => (
+const DisplayCanvas = ({ drawing, width, height, redrawTime=10000, ...props }) => {
+  const canvasRef = createRef(null);
+
+  useEffect(() => {
+    let isActive = true;
+
+    setTimeout(
+      () => {
+        let redrawInterval = setInterval(
+          () => {
+            canvasRef.current?.loadSaveData(drawing);
+            if (!isActive) clearInterval(redrawInterval);
+          },
+          redrawTime + Math.random() * 1000,
+        );
+      },
+      Math.random() * (redrawTime / 2),
+    );
+
+    return () => {
+      isActive = false;
+    };
+  }, [canvasRef, drawing, redrawTime]);
+
+  return (
     <div style={{display: 'flex', justifyContent: 'center', backgroundColor: 'white', borderRadius: '15px', overflow: 'hidden' }}>
       <CanvasDraw 
+        ref={canvasRef}
         brushRadius={3}
         canvasWidth={width}
         canvasHeight={height}
@@ -15,6 +40,7 @@ const DisplayCanvas = ({ drawing, width, height, ...props }) => (
         {...props}
       />
     </div>
-);
+  );
+}
 
 export default DisplayCanvas;
